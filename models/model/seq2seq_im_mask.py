Fixed action loss by properly masking padded values

models/model/seq2seq_im_mask.py
@@ -7,6 +7,8 @@ from torch import nn
 from torch.nn import functional as F
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 from model.seq2seq import Module as Base
+
+from models.utils.loss_utils import masked_mean
 from models.utils.metric import compute_f1, compute_exact
 from gen.utils.image_util import decompress_mask
 
@@ -311,8 +313,7 @@ class Module(Base):
         # action loss
         pad_valid = (l_alow != self.pad)
         alow_loss = F.cross_entropy(p_alow, l_alow, reduction='none')
-        alow_loss *= pad_valid.float()
-        alow_loss = alow_loss.mean()
+        alow_loss = masked_mean(alow_loss, pad_valid, dim=-1)
         losses['action_low'] = alow_loss * self.args.action_loss_wt
 
         # mask loss